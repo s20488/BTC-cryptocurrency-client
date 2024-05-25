86adb95a5cb2e05f9efc69b0c5447d74d972462f
s20488 <s20488@pjwstk.edu.pl>
2024-05-25 12:22:46 +0200

    fix + transaction history + status transaction

--- src/main/java/com/example/btc_project/BitcoinRpcClient.java
+++ src/main/java/com/example/btc_project/BitcoinRpcClient.java
@@ -1,6 +1,8 @@
 package com.example.btc_project;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
 import com.google.zxing.qrcode.QRCodeWriter;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 
@@ -20,9 +22,11 @@ import org.springframework.stereotype.Component;
 
 import javax.imageio.ImageIO;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 @Component
 public class BitcoinRpcClient {
@@ -59,7 +63,7 @@ public class BitcoinRpcClient {
         }
     }
 
-    public List<String> getAddresses() throws IOException {
+    public List<Map<String, String>> getAddresses() throws IOException {
         String requestBody = "{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"getaddressesbylabel\",\"params\":[\"\"]}";
         Request request = new Request.Builder()
                 .url(rpcUrl)
@@ -73,12 +77,40 @@ public class BitcoinRpcClient {
             }
 
             JsonNode responseJson = objectMapper.readTree(response.body().string());
-            List<String> addresses = new ArrayList<>();
-            Iterator<String> addressNames = responseJson.get("result").fieldNames();
-            while (addressNames.hasNext()) {
-                addresses.add(addressNames.next());
+            List<Map<String, String>> addresses = new ArrayList<>();
+            JsonNode result = responseJson.get("result");
+            if (result != null) {
+                Iterator<Map.Entry<String, JsonNode>> addressFields = result.fields();
+                while (addressFields.hasNext()) {
+                    Map.Entry<String, JsonNode> entry = addressFields.next();
+                    String address = entry.getKey();
+                    JsonNode addressData = entry.getValue();
+                    String addressType = addressData.has("purpose") ? addressData.get("purpose").asText() : "unknown";
+                    Map<String, String> addressInfo = new HashMap<>();
+                    addressInfo.put("address", address);
+                    addressInfo.put("type", addressType);
+                    addresses.add(addressInfo);
+                }
             }
             return addresses;
+        }
+    }
+
+    public double getReceivedByAddress(String address) throws IOException {
+        String requestBody = String.format("{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"getreceivedbyaddress\",\"params\":[\"%s\"]}", address);
+        Request request = new Request.Builder()
+                .url(rpcUrl)
+                .addHeader("Authorization", credentials)
+                .post(RequestBody.create(requestBody, MediaType.parse("application/json; charset=utf-8")))
+                .build();
+
+        try (Response response = httpClient.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw new IOException("Unexpected code " + response);
+            }
+
+            JsonNode responseJson = objectMapper.readTree(response.body().string());
+            return responseJson.get("result").asDouble();
         }
     }
 
@@ -122,10 +154,92 @@ public class BitcoinRpcClient {
                 BufferedImage qrCode = MatrixToImageWriter.toBufferedImage(qrCodeWriter.encode(bitcoinUri, BarcodeFormat.QR_CODE, 200, 200));
                 ImageIO.write(qrCode, "PNG", new File("QRCode.png"));
                 System.out.println("QR code successfully generated!");
-            } catch (com.google.zxing.WriterException e) {
+            } catch (WriterException e) {
                 System.err.println("Error while creating a QR code: " + e.getMessage());
             }
             return newAddress;
+        }
+    }
+
+    public List<Map<String, Object>> getTransactionHistory() throws IOException {
+        String requestBody = "{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"listtransactions\",\"params\":[\"*\", 100]}";
+        Request request = new Request.Builder()
+                .url(rpcUrl)
+                .addHeader("Authorization", credentials)
+                .post(RequestBody.create(requestBody, MediaType.parse("application/json; charset=utf-8")))
+                .build();
+
+        try (Response response = httpClient.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw new IOException("Unexpected code " + response);
+            }
+
+            JsonNode responseJson = objectMapper.readTree(response.body().string());
+            List<Map<String, Object>> transactions = new ArrayList<>();
+            JsonNode result = responseJson.get("result");
+            if (result != null && result.isArray()) {
+                for (JsonNode transaction : result) {
+                    Map<String, Object> transactionInfo = new HashMap<>();
+                    long timestamp = transaction.get("time").asLong();
+                    Instant instant = Instant.ofEpochSecond(timestamp);
+                    LocalDateTime dateTime = LocalDateTime.ofInstant(instant, ZoneOffset.UTC);
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+                    String formattedDateTime = dateTime.format(formatter);
+
+                    String amount = transaction.get("amount").asText();
+                    String category = transaction.get("category").asText();
+                    transactionInfo.put("time", formattedDateTime);
+                    transactionInfo.put("amount", amount);
+                    transactionInfo.put("category", category);
+                    transactions.add(transactionInfo);
+                }
+            }
+            return transactions;
+        }
+    }
+
+    public void monitorTransactionConfirmations(String txid) throws IOException, InterruptedException {
+        final int requiredConfirmations = 6;
+        String requestBody = "{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"gettransaction\",\"params\":[\"" + txid + "\"]}";
+        Request request = new Request.Builder()
+                .url(rpcUrl)
+                .addHeader("Authorization", credentials)
+                .post(RequestBody.create(requestBody, MediaType.parse("application/json; charset=utf-8")))
+                .build();
+
+        while (true) {
+            try (Response response = httpClient.newCall(request).execute()) {
+                if (!response.isSuccessful()) {
+                    throw new IOException("Unexpected code " + response);
+                }
+
+                JsonNode responseJson = objectMapper.readTree(response.body().string());
+                JsonNode result = responseJson.get("result");
+                if (result != null) {
+                    int confirmations = result.get("confirmations").asInt();
+                    String status = "";
+                    if (confirmations == 0) {
+                        status = "Unconfirmed";
+                    } else if (confirmations < requiredConfirmations) {
+                        status = "Pending";
+                    } else if (confirmations >= requiredConfirmations && confirmations < 10) {
+                        status = "Confirmed";
+                    }
+
+                    System.out.println("Transaction ID: " + txid);
+                    System.out.println("Confirmations: " + confirmations);
+                    System.out.println("Status: " + status);
+
+                    if (confirmations >= requiredConfirmations) {
+                        System.out.println("Transaction has reached the required number of confirmations.");
+                        break;
+                    }
+                } else {
+                    System.out.println("Transaction not found.");
+                    break;
+                }
+            }
+            Thread.sleep(60000);
         }
     }
 }
--- src/main/java/com/example/btc_project/BtcProjectApplication.java
+++ src/main/java/com/example/btc_project/BtcProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.text.DecimalFormat;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 @SpringBootApplication
@@ -26,23 +27,27 @@ public class BtcProjectApplication implements CommandLineRunner {
 
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Choose operation:");
-		System.out.println("1. Get a list of addresses");
-		System.out.println("2. Check wallet balance");
-		System.out.println("3. Send funds");
-		System.out.println("4. Get new address");
+		System.out.println("1. Balance ");
+		System.out.println("2. Addresses");
+		System.out.println("3. Send");
+		System.out.println("4. Receive");
+		System.out.println("5. Transaction history");
+		System.out.println("6. Status transaction");
 		int choice = scanner.nextInt();
 		double amount = scanner.nextDouble();
 
 		switch (choice) {
 			case 1:
-				List<String> addresses = bitcoinRpcClient.getAddresses();
-				System.out.println("List of available addresses:");
-				for (String address : addresses) {
-					System.out.println(address);
-				}
+				System.out.println("Balance: " + df.format(balance) + " BTC");
 				break;
 			case 2:
-				System.out.println("Balance: " + df.format(balance) + " BTC");
+				List<Map<String, String>> addresses = bitcoinRpcClient.getAddresses();
+				for (Map<String, String> addressInfo : addresses) {
+					String address = addressInfo.get("address");
+					String type = addressInfo.get("type");
+					double balanceAddress = bitcoinRpcClient.getReceivedByAddress(address);
+					System.out.println("Address: " + address + ", Type: " + type + ", Balance: " + df.format(balanceAddress));
+				}
 				break;
 			case 3:
 				System.out.println("Enter the recipient's address:");
@@ -63,6 +68,20 @@ public class BtcProjectApplication implements CommandLineRunner {
 				System.out.println("Enter the desired amount:");
 				String newAddress = bitcoinRpcClient.getNewAddressAndGenerateQRCode(amount);
 				System.out.println("Your new address: " + newAddress + ". QR code with the amount " + amount + " BTC was successfully generated!");
+				break;
+			case 5:
+				List<Map<String, Object>> transactions = bitcoinRpcClient.getTransactionHistory();
+				for (Map<String, Object> transaction : transactions) {
+					String time = transaction.get("time").toString();
+					double amountTransaction = Double.parseDouble(transaction.get("amount").toString());
+					String category = transaction.get("category").toString();
+					System.out.println("Time: " + time + ", Amount: " + df.format(amountTransaction) + ", Category: " + category);
+				}
+				break;
+			case 6:
+				System.out.println("Enter transaction ID to monitor:");
+				String txid = scanner.next();
+				bitcoinRpcClient.monitorTransactionConfirmations(txid);
 				break;
 			default:
 				System.out.println("Incorrect operation selection");
